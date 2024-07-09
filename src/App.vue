0762c78e31c5c1877ec2cbed935da8a8d9005b63
barelyfalse <hyp3rbone@outlook.com>
2024-07-09 17:44:56 -0600

    button changed + ✨

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import type { Segment, Block, ParseResult } from '@/dynamicListParser'
 
 import Container from './components/Container.vue'
 import PatternInput from './components/PatternInput.vue'
-import Button, { buttonTypes } from './components/Button.vue'
+import ButtonFilled from './components/ButtonFilled.vue'
 import BlockGroup from './components/BlockGroup.vue'
 
 const patternInputValue = ref('{-w: "-w",-n-r}')
@@ -14,65 +14,78 @@ const parsedPattern = ref(createResult(true, true, 'Submit a pattern'))
 const plainOutputValue = ref('')
 const outputTextareaRef = ref(null)
 
-function processPatternInput(pattern:string):void {
+function processPatternInput(pattern: string): void {
   patternInputValue.value = pattern
 }
 
-function processPattern():void {
-  parsedPattern.value = dynamicListParser(patternInputValue.value.replace (/\n/g,'-n'))
+function processPattern(): void {
+  parsedPattern.value = dynamicListParser(
+    patternInputValue.value.replace(/\n/g, '-n')
+  )
   //console.log(parsedPattern.value)
 }
 
 function updateRootValues(newValues: any[]): void {
   if (parsedPattern.value.root) {
-    parsedPattern.value.root.values = newValues;
+    parsedPattern.value.root.values = newValues
     //console.log('Root values updated:', parsedPattern.value.root.values);
     plainOutputValue.value = renderPlainText(parsedPattern.value.root)
   }
 }
 
 const isBlock = (item: Block | Segment): item is Block => {
-  return 'children' in item;
-};
+  return 'children' in item
+}
 
 const renderPlainText = (block: Block): string => {
-  return block.values.map(item => {
-    if (isBlock(item)) {
-      return `${item.start}${renderPlainText(item)}${item.end}`
-    } else {
-      //console.log(item.value||item.raw)
-      return item.value || item.raw;
-    }
-  }).join('');
-};
+  return block.values
+    .map((item) => {
+      if (isBlock(item)) {
+        return `${item.start}${renderPlainText(item)}${item.end}`
+      } else {
+        //console.log(item.value||item.raw)
+        return item.value || item.raw
+      }
+    })
+    .join('')
+}
 
 const plainTextOutput = computed(() => {
-  return parsedPattern.value.root&&renderPlainText(parsedPattern.value.root)
-});
+  return parsedPattern.value.root && renderPlainText(parsedPattern.value.root)
+})
 
 watch(plainOutputValue, () => {
   if (outputTextareaRef.value) {
-    (outputTextareaRef.value as HTMLInputElement).style.height = 'auto';
-    (outputTextareaRef.value as HTMLInputElement).style.height = `${(outputTextareaRef.value as HTMLInputElement).scrollHeight + 32}px`
+    ;(outputTextareaRef.value as HTMLInputElement).style.height = 'auto'
+    ;(outputTextareaRef.value as HTMLInputElement).style.height = `${
+      (outputTextareaRef.value as HTMLInputElement).scrollHeight + 32
+    }px`
   }
 })
-
 </script>
 
 <template>
   <header>
     <h1># list crafter</h1>
     <div>
-      <PatternInput :value="patternInputValue" helper-text="" :on-input="processPatternInput" fit-width />
-      <div style="display: flex; flex-direction: row; gap: 8px;">
-        <Button label="Parse" :type="buttonTypes.filled" :action="processPattern" />
-        <Button label="Clear" :type="buttonTypes.filled" :action="() => {}" />
+      <PatternInput
+        :value="patternInputValue"
+        helper-text=""
+        :on-input="processPatternInput"
+        fit-width
+      />
+      <div style="display: flex; flex-direction: row; gap: 8px">
+        <button-filled
+          label="Parse"
+          :action="processPattern"
+        />
+        <button-filled label="Clear" :action="() => {}" />
       </div>
     </div>
   </header>
 
   <main>
-    <br>
+    <br />
     <h2>## parsed pattern</h2>
     <Container>
       <template #content>
@@ -85,9 +98,14 @@ watch(plainOutputValue, () => {
         <p v-else>{{ parsedPattern.message }}</p>
       </template>
     </Container>
-    <br>
+    <br />
     <h2>## parsed list</h2>
-    <textarea class="plain-textarea" ref="outputTextareaRef" v-model="plainOutputValue" readonly></textarea>
+    <textarea
+      class="plain-textarea"
+      ref="outputTextareaRef"
+      v-model="plainOutputValue"
+      readonly
+    ></textarea>
   </main>
 </template>
 
@@ -107,6 +125,5 @@ watch(plainOutputValue, () => {
   border: 1px solid var(--outline-variant)
   border-radius: 8px
   &:focus
-    outline: none 
-
+    outline: none
 </style>
