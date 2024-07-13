31de9fc1fc692fc1aca158ffed751f12fd774231
barelyfalse <hyp3rbone@outlook.com>
2024-07-13 01:43:57 -0600

    gh pages vite base

--- vite.config.ts
+++ vite.config.ts
@@ -15,5 +15,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: '/list-crafter/'
+  base: 'https://barelyfalse.github.io/list-crafter/'
 })
